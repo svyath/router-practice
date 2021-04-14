b6d5eaf9a5bf5ceb728be78e9465549b25eb8179
Svyat <43937124+Svyatyk@users.noreply.github.com>
2021-04-14 14:48:44 +0300

    skeleton adding

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Shop Logo</h3>
+        <ul className="navLinks">
+            <li>About</li>
+            <li>Shop</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
